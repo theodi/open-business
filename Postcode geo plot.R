e97c9e8aed815aafa9fa8e0568ee259708165f0e
Jmerules <Jamie@Jamies-MacBook-Air.local>
2014-10-31 11:33:52 +0000

    Working map plotting code

--- Postcode geo plot.R
+++ Postcode geo plot.R
@@ -6,97 +6,176 @@ write.csv(list[, "Postcode"], "master-postcodes.csv", , row.names = FALSE)
 
 setwd("~/git/open-business")
 theme_set(theme_minimal(base_family = "Helvetica Neue", base_size = 18))
-source('/Users/Jamie/Documents/R STUDIO/Open Business/ODI colour Scheme.R')
+source('~/git/open-business/ODI colour Scheme.R')
 library(ggplot2)
 library(ggmap)
 
 options(stringsAsFactors = FALSE)
 
 
-#---------------------------------------------
-#Get map
+#==========================================================
+#Get the map
+#==========================================================
 
-#bounding box but google does not interpret this very well - others might do better
-# Might be best: (-8.459, 49.909, 2.175, 59.623)
-# To generate coordinates on a map use osm export function and copy values
-UKbox.g <- c(-8.6, 49, 1.7, 60)
-UKbox <- c(-8.459, 49.909, 2.175, 59.623)
+#Google Maps version - currently working
+#Google specific - bounding box for UK map 
+UKbox.g <- c(-8.6, 49, 1.7, 60) #google does not interpret bboxes very well so tweaked version
 
-#This uses google maps but is alright for now - 
-#explore other options - play with other maps and zooms and bounding box coords
-UKmap <- get_map(UKbox.g,
+UKmap.g <- get_map(UKbox.g,
                  zoom = 6,
                  maptype = c("terrain"),
                  messaging = FALSE, 
                  crop = FALSE,
-                 color = c("color"),
+                 color = c("bw"),
                  source = c("google"))
 
 #Plot the map to variable UKMap
-UKMap <- ggmap(UKmap, extent = 'device', legend = 'topleft')
+UKMap.g <- ggmap(UKmap.g, extent = 'device')
+print(UKMap.g)
 
-print(UKMap)
-#OSM - currently not working properly! maybe doesn't want to export such a large map - can't work out correct scale
-UKmap <- get_map(UKbox,
+
+# Get a map of London
+Lonmap.g <- get_map("London, UK",
+                   zoom = 12,
+                   maptype = c("terrain"),
+                   messaging = FALSE, 
+                   crop = FALSE,
+                   color = c("bw"),
+                   source = c("google"))
+
+#Plot the map to variable UKMap
+LonMap.g <- ggmap(Lonmap.g, extent = 'device')
+print(LonMap.g)
+
+#---------------------------------------------
+#bounding box for other maps
+# To generate coordinates on a map use osm export function and copy values
+UKbox <- c(-8.613, 49.866, 1.934, 58.995)
+
+#Stamen 
+# Get a UK map
+UKmap.s <- get_map(UKbox,
                  zoom = 7,
                  maptype = c("toner"),
-                 filename = "ggmapTemp", crop = FALSE,
-                 color = c("bw"),
+                 crop = FALSE,
+                 color = c("color"),
                  source = c("stamen"),
                 )
 
+#Plot the map to variable UKMap
+UKMap.s <- ggmap(UKmap.s, extent = 'device', legend = 'topleft')
+print(UKMap.s)
 
-       
+# Get a London Map
+
+Lonmap.s <- get_map("London, UK",
+                   zoom = 12,
+                   maptype = c("toner"),
+                   crop = FALSE,
+                   color = c("color"),
+                   source = c("stamen"),
+                   )
+
+LonMap.s <- ggmap(Lonmap.s, extent = 'device', legend = 'topleft')
+print(LonMap.s)
+
 
 #---------------------------------------------
+
+#OSM - NOT WORKING
+#UK Map - not working
+UKmap.o <- get_map(UKbox,
+                 scale = "auto",
+                 crop = FALSE,
+                 color = c("color"),
+                 source = c("osm"),
+                 )
+   
+#Plot the map to variable UKMap
+UKMap.o <- ggmap(UKmap.o, extent = 'device', legend = 'topleft')
+print(UKMap.o)
+
+# Get a London Map - Not working
+
+Lonmap.o <- get_map("London, UK",
+                    zoom = 12,
+                    maptype = c("toner"),
+                    crop = FALSE,
+                    color = c("color"),
+                    source = c("osm"),
+                    )
+
+LonMap.o <- ggmap(Lonmap.o, extent = 'device', legend = 'topleft')
+print(LonMap.o)
+
+#==========================================================
 # Setup data points to map
+#==========================================================
 
 # Read the exported csv
-postcodes <- read.csv("master-postcodes.csv")
+postcodes.full <- read.csv("master-postcodes.csv")
 
 # Clean several addresses - remove "herts" from one postcode, correct 2 others - *correct this in master when time*
-postcodes$x <- gsub("Herts ", "", postcodes$x)
-postcodes$x <- gsub("EC24A 4JE", "EC2A 4JE", postcodes$x)
+postcodes.full$x <- gsub("Herts ", "", postcodes.full$x)
+postcodes.full$x <- gsub("EC24A 4JE", "EC2A 4JE", postcodes.full$x)
+
+# Remove NAs - as NA plots at lon=18.4904100  lat=-22.95764 (assume North America or state but doesnt plot on our map anyway)
+postcodes <- postcodes.full$x[!is.na(postcodes.full$x)]
 
 #Convert postcodes to lon/lat of places
-#NA plots at lon=18.4904100  lat=-22.95764 (assume North America or state but doesnt plot on our map anyway)
-place <- geocode(postcodes$x)
+place <- geocode(postcodes)
 
-
-#failed method
-#post.frame <- read.csv("master-postcodes.csv")
-#name column 'postcode'
-#names(post.frame)[names(post.frame)=="x"] <- "postcode"
-#post.frame$postcode <- gsub("Herts ", "", post.frame$postcode)
-#post.frame$postcode <- gsub("EC24A 4JE", "EC2A 4JE", post.frame$postcode)
+#write csv to avoid having to rerun Google API calls!
+write.csv(place, "lon-lats.csv", , row.names = FALSE)
 
 
 #TESTS - done with one value then while offline
 #aplace <- geocode("EC2A 4JE")
 #print(aplace)
 #print(as.numeric(aplace))
+#Test done with small matrix while offline
 #test.lon <- c(-0.0836629, -0.5, 0, 0.1, -1)
 #test.lat <- c(51.52239, 52, 50.5, 51, 53)
 #aplace.test <- data.frame(test.lon, test.lat)
 
-
 #---------------------------------------------
+#If using the 260 postcodes already identified
+
+place <- read.csv("lon-lats.csv")
+
+
+#==========================================================
 # Add points to map
+#==========================================================
 
-#Plot of points 
- UKMap+ 
-  geom_point(aes( x = lon, y = lat), data = place,
-            alpha= 1, color = "orange", na.rm = TRUE)
+#Plot of points to UKmap
+# On gmap
+UKMap.g+ 
+  geom_point(aes(x = lon, y = lat), data = place,
+             alpha= 1, color = odi_orange)
+ggsave("graphics/UKMap-google.png", height = 4, width = 12)
 
+# On stamen
+UKMap.s+ 
+  geom_point(aes(x = lon, y = lat), data = place,
+             alpha= 1, color = odi_orange)
+ggsave("graphics/UKMap-Stamen-toner.png", height = 4, width = 12)
+#---------------------------------------------
+#Plots of points on London map
 
+# On gmap
+LonMap.g+ 
+  geom_point(aes(x = lon, y = lat), data = place,
+             alpha= 1, color = odi_orange)
+ggsave("graphics/LonMap-google.png", height = 4, width = 12)
+
+# On stamen
+LonMap.s+ 
+  geom_point(aes(x = lon, y = lat), data = place,
+             alpha= 1, color = odi_orange)
+ggsave("graphics/LonMap-Stamen-toner.png", height = 4, width = 12)
+#---------------------------------------------
 # PLot of density - heatmap - this is not working - YET!!
-
-UKMap + stat_density2d(aes(x = lon, y = lat, 
-                     fill = ..level.. , alpha = ..level..),size = 2, bins = 4, 
-                 data = place, geom = 'polygon', color = 'orange') 
-      + scale_fill_gradient('aplace.test') 
-      + scale_alpha(range = c(.4, .75), guide = FALSE) 
-      + guides(fill = guide_colorbar(barwidth = 1.5, barheight = 10))
 
 
 
